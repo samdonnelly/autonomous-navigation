d33f7d325fbb724d5a4a5cca242790a49149c94d
Sam <samueldonnelly11@gmail.com>
2025-02-21 22:58:50 -0700

    Started reformatting the code to separate hardware and autopilot code.

---
+++ auto_nav/autopilot/vehicle/vehicle.h
@@ -0,0 +1,35 @@
+/**
+ * @file vehicle.h
+ * 
+ * @author Sam Donnelly (samueldonnelly11@gmail.com)
+ * 
+ * @brief Vehicle interface 
+ * 
+ * @details Properties common to all vehicles. 
+ * 
+ * @version 0.1
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#ifndef _VEHICLE_H_ 
+#define _VEHICLE_H_ 
+
+//=======================================================================================
+// Includes 
+//=======================================================================================
+
+
+//=======================================================================================
+// Classes 
+
+class Vehicle 
+{
+    // 
+}; 
+
+//=======================================================================================
+
+#endif   // _VEHICLE_H_ 
---
+++ auto_nav/autopilot/vehicle/vehicle_mavlink.h
@@ -0,0 +1,35 @@
+/**
+ * @file vehicle_mavlink.h
+ * 
+ * @author Sam Donnelly (samueldonnelly11@gmail.com)
+ * 
+ * @brief Vehicle MAVLink interface 
+ * 
+ * @details MAVLink messages and properites common to all vehicles. 
+ * 
+ * @version 0.1
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#ifndef _VEHICLE_MAVLINK_H_ 
+#define _VEHICLE_MAVLINK_H_ 
+
+//=======================================================================================
+// Includes 
+//=======================================================================================
+
+
+//=======================================================================================
+// Classes 
+
+class VehicleMAVLink 
+{
+    // 
+}; 
+
+//=======================================================================================
+
+#endif   // _VEHICLE_MAVLINK_H_ 
