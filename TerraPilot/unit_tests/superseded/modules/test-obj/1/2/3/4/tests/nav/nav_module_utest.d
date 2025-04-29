225e0861e09223f8c4e14321cff9a9d1cb66f6cd
Sam Donnelly <samueldonnelly11@gmail.com>
2025-04-29 11:17:03 -0600

    Reformatted file structure.

---
+++ TerraPilot/unit_tests/superseded/modules/test-obj/1/2/3/4/tests/nav/nav_module_utest.d
@@ -0,0 +1,54 @@
+test-obj/1/2/3/4/tests/nav/nav_module_utest.o: \
+ tests/nav/nav_module_utest.cpp \
+ ../../../../cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ ../../../../cpputest/include/CppUTest/CppUTestConfig.h \
+ ../../../../cpputest/include/CppUTest/CppUTestGeneratedConfig.h \
+ ../../../../cpputest/include/CppUTest/StandardCLibrary.h \
+ ../../../../cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
+ ../../../../cpputest/include/CppUTest/TestHarness.h \
+ ../../../../cpputest/include/CppUTest/CppUTestConfig.h \
+ ../../../../cpputest/include/CppUTest/Utest.h \
+ ../../../../cpputest/include/CppUTest/SimpleString.h \
+ ../../../../cpputest/include/CppUTest/StandardCLibrary.h \
+ ../../../../cpputest/include/CppUTest/UtestMacros.h \
+ ../../../../cpputest/include/CppUTest/TestResult.h \
+ ../../../../cpputest/include/CppUTest/TestFailure.h \
+ ../../../../cpputest/include/CppUTest/TestPlugin.h \
+ ../../../../cpputest/include/CppUTest/MemoryLeakWarningPlugin.h \
+ ../../../../cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ ../../headers/modules/nav_module.h \
+ ../../../../STM32F4-driver-library/headers/tools/tools.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/core_cm4.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_version.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_compiler.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_gcc.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/mpu_armv7.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../../../../STM32F4-driver-library/headers/tools/nav_calcs.h
+../../../../cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+../../../../cpputest/include/CppUTest/CppUTestConfig.h:
+../../../../cpputest/include/CppUTest/CppUTestGeneratedConfig.h:
+../../../../cpputest/include/CppUTest/StandardCLibrary.h:
+../../../../cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
+../../../../cpputest/include/CppUTest/TestHarness.h:
+../../../../cpputest/include/CppUTest/CppUTestConfig.h:
+../../../../cpputest/include/CppUTest/Utest.h:
+../../../../cpputest/include/CppUTest/SimpleString.h:
+../../../../cpputest/include/CppUTest/StandardCLibrary.h:
+../../../../cpputest/include/CppUTest/UtestMacros.h:
+../../../../cpputest/include/CppUTest/TestResult.h:
+../../../../cpputest/include/CppUTest/TestFailure.h:
+../../../../cpputest/include/CppUTest/TestPlugin.h:
+../../../../cpputest/include/CppUTest/MemoryLeakWarningPlugin.h:
+../../../../cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+../../headers/modules/nav_module.h:
+../../../../STM32F4-driver-library/headers/tools/tools.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/core_cm4.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_version.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_compiler.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_gcc.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/mpu_armv7.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../../../../STM32F4-driver-library/headers/tools/nav_calcs.h:
---
+++ TerraPilot/unit_tests/superseded/modules/test-obj/1/2/sources/modules/nav_module.d
@@ -0,0 +1,32 @@
+test-obj/1/2/3/4/./../../sources/modules/nav_module.o: \
+ ../../sources/modules/nav_module.cpp \
+ ../../../../cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ ../../../../cpputest/include/CppUTest/CppUTestConfig.h \
+ ../../../../cpputest/include/CppUTest/CppUTestGeneratedConfig.h \
+ ../../../../cpputest/include/CppUTest/StandardCLibrary.h \
+ ../../../../cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
+ ../../headers/modules/nav_module.h \
+ ../../../../STM32F4-driver-library/headers/tools/tools.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/core_cm4.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_version.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_compiler.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_gcc.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/mpu_armv7.h \
+ ../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../../../../STM32F4-driver-library/headers/tools/nav_calcs.h
+../../../../cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+../../../../cpputest/include/CppUTest/CppUTestConfig.h:
+../../../../cpputest/include/CppUTest/CppUTestGeneratedConfig.h:
+../../../../cpputest/include/CppUTest/StandardCLibrary.h:
+../../../../cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
+../../headers/modules/nav_module.h:
+../../../../STM32F4-driver-library/headers/tools/tools.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/core_cm4.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_version.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_compiler.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/cmsis_gcc.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Core/Include/mpu_armv7.h:
+../../../../STM32F4-driver-library/stmcode/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../../../../STM32F4-driver-library/headers/tools/nav_calcs.h:
